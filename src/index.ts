ae5c6d74d4e836bd5c5b8770e5f0387a6a6fef24
Digerati <cabal@digerati.design>
2024-02-03 14:11:00 +0000

    Add modal class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-import { greetUser } from '$utils/greet';
+import { modal } from '$dg/modal';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  modal();
 });
